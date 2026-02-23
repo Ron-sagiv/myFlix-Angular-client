8de77a8f689c5e18deca0e7c903c7461be0e12c7
Ron-sagiv <pedrozeeg@gmail.com>
2026-02-23 18:57:34 +0100

    Added movie card and welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,38 @@
 // src/app/app.component.ts
+// import { Component } from '@angular/core';
+// import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+// import { MatDialog } from '@angular/material/dialog';
+// import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+// import { MovieCardComponent } from './movie-card/movie-card.component';
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss'],
+// })
+// export class AppComponent {
+//   title = 'myFlix-Angular-client';
+
+//   constructor(public dialog: MatDialog) {}
+
+//   openUserRegistrationDialog(): void {
+//     this.dialog.open(UserRegistrationFormComponent, {
+
+//       width: '280px',
+//     });
+//   }
+//   openUserLoginDialog(): void {
+//     this.dialog.open(UserLoginFormComponent, {
+//       width: '280px',
+//     });
+//   }
+//   openMoviesDialog(): void {
+//     this.dialog.open(MovieCardComponent, {
+//       width: '500px',
+//     });
+//   }
+// }
+
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { MatDialog } from '@angular/material/dialog';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +41,4 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  constructor(public dialog: MatDialog) {}
-  // This is the function that will open the dialog when the signup button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      // Assigning the dialog a width
-      width: '280px',
-    });
-  }
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: '280px',
-    });
-  }
 }
